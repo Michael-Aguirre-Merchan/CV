d4e6dec939016b04e23769447ffe90985550067e
Canuto2 <maikol.merchan23@gmail.com>
2021-01-25 12:09:14 -0700

    everything ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HistoryComponent } from './history/history.component';
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'home', component: HomeComponent},
+  {path: 'play', component: PlayComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'history', component: HistoryComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: '' , redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgxChessBoardModule } from "ngx-chess-board";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
+import { AboutComponent } from './about/about.component';
+import { HistoryComponent } from './history/history.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    PlayComponent,
+    AboutComponent,
+    HistoryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxChessBoardModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
