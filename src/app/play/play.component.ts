10077826f4209a74ccc9ab55ac85e61046a78861
Canuto2 <maikol.merchan23@gmail.com>
2021-02-05 12:37:01 -0700

    Responsive - Some fixes

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,11 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
   styleUrls: ['./play.component.css']
 })
-export class PlayComponent implements OnInit {
+export class PlayComponent 
+// implements AfterViewInit 
+    {
+
+
+  // @ViewChild('divToMeasure') divToMeasureElement!: ElementRef;
+
+   public size:any;
+
+  // ngAfterViewInit() {
+
+  //   let divToMeasureWidth = this.divToMeasureElement.nativeElement.offsetWidth;
+
+  //   console.log(divToMeasureWidth);
+
+  //   this.size = divToMeasureWidth
+
+  //   console.log(this.size)
+     
+  // }
+
+     
+  
+  
+  
 
   constructor() { }
 
