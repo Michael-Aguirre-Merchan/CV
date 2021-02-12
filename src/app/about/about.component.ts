63e1bba15b6ad0befc600f33c4e2c32c04fec776
Canuto2 <maikol.merchan23@gmail.com>
2021-02-12 00:04:26 -0500

    Animations About - Part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -42,9 +42,16 @@ export class AboutComponent implements OnInit {
     a = document.getElementById(read.id);
     b = document.getElementById("card-deck");
     
+    setTimeout( function(){
     b.style.display = "flex";
     a.style.display = "none"
+  }, 1500 );
 
+  a.style.opacity = 0
+
+  setTimeout( function(){
+    b.style.opacity = 1
+  }, 2000 );
   }
 
   cardCode(card:any) {
@@ -57,13 +64,59 @@ export class AboutComponent implements OnInit {
     x = document.getElementById("card-deck");
     y = document.getElementById(z);
 
-      if (x.style.display === "none") {
-        x.style.display = "flex";
-        y.style.display = "none"
-      } else {
-        x.style.display = "none";
-        y.style.display = "block"
-      }
+    setTimeout( function(){
+      x.style.display = "none";
+      y.style.display = "block"
+    }, 1500 );
+      
+      // t.style.animation = "myfirst 5s linear forwards";
+      x.style.opacity = 0
+
+      setTimeout( function(){
+        y.style.opacity = 1
+      }, 2000 );
+      
     };
+
+    animationTest(card:any) {
+
+      
+
+      
+
+//       var element = document.getElementsByName("element");
+//       var values= '';
+//       for(var i=0; i<element.length; i++) {
+//         // console.log(element[i].id);
+//         // var test = element[i].id;
+//         // console.log(test);
+        
+//         // if(element[i].id === card.id) {
+//       values+= element[i].id;
+//     // }
+// }
+// console.log(values);
+// var val = [1, 2, 3, 4, 5, 6]
+// for( var i = 0; i < val.length; i++){ 
+                                   
+//    if ( val[i] === card.id) { 
+//        val.splice(i, 1); 
+//        i--; 
+//    }   
+//  }
+//  console.log(val);
+//  var valu:any
+//  valu = val.map(String)
+//  var elements:any
+// //  console.log(elements);
+ 
+//  for( var x = 0; x < valu.length; x++ ) {
+//   elements+= document.getElementById(valu[x])!.id
+//   ;
+// }
+// console.log(elements);
+
+ 
+    }
 
 }
