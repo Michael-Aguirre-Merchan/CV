8e6c6550d0600b1361b8123707dea1e533057891
Canuto2 <maikol.merchan23@gmail.com>
2021-02-14 22:42:21 -0500

    Animations About - Part 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,115 +8,89 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
   cards = [
-    {id: 1, title: 'Personal Info', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 1'},
-    {id: 2, title: 'Skills', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 2'},
-    {id: 3, title: 'Interests', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 3'},
-    {id: 4, title: 'Web Development', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 4'},
-    {id: 5, title: 'Other Certifications', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 5'},
-    {id: 6, title: 'Other Participations', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 6'}
-      ];
-  
+    { id: 1, title: 'Personal Info', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 1' },
+    { id: 2, title: 'Skills', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 2' },
+    { id: 3, title: 'Interests', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 3' },
+    { id: 4, title: 'Web Development', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 4' },
+    { id: 5, title: 'Other Certifications', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 5' },
+    { id: 6, title: 'Other Participations', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 6' }
+  ];
+
   reads = [
 
-    {id: 'card1', title: 'Personal Info', text: 'Lorem ipsum dolor sit amet, consetetur'},
-    {id: 'card2', title: 'Skills', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr'},
-    {id: 'card3', title: 'Interests', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr,'},
-    {id: 'card4', title: 'Web Development', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr,'},
-    {id: 'card5', title: 'Other Certifications', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr,'},
-    {id: 'card6', title: 'Other Participations', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed'}
+    { id: 'card1', title: 'Personal Info', text: 'Lorem ipsum dolor sit amet, consetetur' },
+    { id: 'card2', title: 'Skills', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr' },
+    { id: 'card3', title: 'Interests', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr,' },
+    { id: 'card4', title: 'Web Development', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr,' },
+    { id: 'card5', title: 'Other Certifications', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr,' },
+    { id: 'card6', title: 'Other Participations', text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed' }
 
   ]
- 
+
   constructor() {
 
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  moreCardCode(read:any) {
+  moreCardCode(read: any) {
 
-    var a:any;
-    var b:any;
+    var a: any;
+    var b: any;
+    var c: any;
 
     a = document.getElementById(read.id);
     b = document.getElementById("card-deck");
-    
-    setTimeout( function(){
-    b.style.display = "flex";
-    a.style.display = "none"
-  }, 1500 );
+    c = document.getElementById("footer")
 
-  a.style.opacity = 0
+    c.style.opacity = 0
+    setTimeout(function () {
+      b.style.display = "flex";
+      a.style.display = "none"
+    }, 1500);
 
-  setTimeout( function(){
-    b.style.opacity = 1
-  }, 2000 );
+    a.style.opacity = 0
+
+    setTimeout(function () {
+      b.style.opacity = 1
+      c.style.opacity = 1
+      c.style.position = "absolute";
+      c.style.width = "91.6666666667%";
+      c.style.bottom = "0";
+    }, 2000);
+
   }
 
-  cardCode(card:any) {
-    var x:any; 
-    var y:any;
-    var z:any;
+  cardCode(card: any) {
+    
+    var x: any;
+    var y: any;
+    var z: any;
+    var yz: any;
 
-    z = 'card'+card.id
-
+    z = 'card' + card.id
     x = document.getElementById("card-deck");
     y = document.getElementById(z);
+    yz = document.getElementById("footer");
 
-    setTimeout( function(){
+    setTimeout(function () {
       x.style.display = "none";
       y.style.display = "block"
-    }, 1500 );
-      
-      // t.style.animation = "myfirst 5s linear forwards";
-      x.style.opacity = 0
+    }, 1500);
 
-      setTimeout( function(){
-        y.style.opacity = 1
-      }, 2000 );
-      
-    };
+    yz.style.opacity = 0
+    x.style.opacity = 0
 
-    animationTest(card:any) {
+    setTimeout(function () {
+      yz.style.opacity = 1
+      y.style.opacity = 1
+      yz.style.position = "fixed";
+    }, 2000);
 
-      
+    yz.style.bottom = "2%";
+    yz.style.width = "74%";
 
-      
-
-//       var element = document.getElementsByName("element");
-//       var values= '';
-//       for(var i=0; i<element.length; i++) {
-//         // console.log(element[i].id);
-//         // var test = element[i].id;
-//         // console.log(test);
-        
-//         // if(element[i].id === card.id) {
-//       values+= element[i].id;
-//     // }
-// }
-// console.log(values);
-// var val = [1, 2, 3, 4, 5, 6]
-// for( var i = 0; i < val.length; i++){ 
-                                   
-//    if ( val[i] === card.id) { 
-//        val.splice(i, 1); 
-//        i--; 
-//    }   
-//  }
-//  console.log(val);
-//  var valu:any
-//  valu = val.map(String)
-//  var elements:any
-// //  console.log(elements);
- 
-//  for( var x = 0; x < valu.length; x++ ) {
-//   elements+= document.getElementById(valu[x])!.id
-//   ;
-// }
-// console.log(elements);
-
- 
-    }
+  };
 
 }
