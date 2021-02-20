749693e448d96bbade9476aaef4db30f9f5296aa
Canuto2 <maikol.merchan23@gmail.com>
2021-02-19 23:48:56 -0500

    Fix CV

--- src/dist/chess/vendor.js
+++ src/dist/chess/vendor.js
@@ -14027,6 +14027,198 @@ function emptyScheduled(scheduler) {
 
 /***/ }),
 
+/***/ "EnSh":
+/*!***************************************************************************!*\
+  !*** ./node_modules/ngx-filesaver/__ivy_ngcc__/fesm2015/ngx-filesaver.js ***!
+  \***************************************************************************/
+/*! exports provided: FileSaverDirective, FileSaverModule, FileSaverService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FileSaverDirective", function() { return FileSaverDirective; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FileSaverModule", function() { return FileSaverModule; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FileSaverService", function() { return FileSaverService; });
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var file_saver__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! file-saver */ "Iab2");
+/* harmony import */ var file_saver__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(file_saver__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _angular_common_http__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common/http */ "tk/3");
+/* harmony import */ var rxjs__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! rxjs */ "qCKp");
+
+
+
+
+
+
+
+class FileSaverService {
+    get isFileSaverSupported() {
+        try {
+            return !!new Blob();
+        }
+        catch (e) {
+            return false;
+        }
+    }
+    genType(fileName) {
+        if (!fileName || fileName.lastIndexOf('.') === -1) {
+            return 'text/plain';
+        }
+        const type = fileName.substr(fileName.lastIndexOf('.') + 1);
+        switch (type) {
+            case 'txt':
+                return 'text/plain';
+            case 'xml':
+            case 'html':
+                return `text/${type}`;
+            case 'json':
+                return 'octet/stream';
+            default:
+                return `application/${type}`;
+        }
+    }
+    save(blob, fileName, filtType, option) {
+        if (!blob) {
+            throw new Error('Data argument should be a blob instance');
+        }
+        const file = new Blob([blob], { type: filtType || blob.type || this.genType(fileName) });
+        Object(file_saver__WEBPACK_IMPORTED_MODULE_1__["saveAs"])(file, decodeURI(fileName || 'download'), option);
+    }
+    saveText(txt, fileName, option) {
+        const blob = new Blob([txt]);
+        this.save(blob, fileName, null, option);
+    }
+}
+FileSaverService.ɵfac = function FileSaverService_Factory(t) { return new (t || FileSaverService)(); };
+FileSaverService.ɵprov = Object(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjectable"])({ factory: function FileSaverService_Factory() { return new FileSaverService(); }, token: FileSaverService, providedIn: "root" });
+/*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](FileSaverService, [{
+        type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Injectable"],
+        args: [{ providedIn: 'root' }]
+    }], null, null); })();
+
+class FileSaverDirective {
+    constructor(el, fss, httpClient) {
+        this.el = el;
+        this.fss = fss;
+        this.httpClient = httpClient;
+        this.method = 'GET';
+        this.success = new _angular_core__WEBPACK_IMPORTED_MODULE_0__["EventEmitter"]();
+        this.error = new _angular_core__WEBPACK_IMPORTED_MODULE_0__["EventEmitter"]();
+        if (!fss.isFileSaverSupported) {
+            el.nativeElement.classList.add(`filesaver__not-support`);
+        }
+    }
+    getName(res) {
+        return decodeURI(this.fileName || res.headers.get('filename') || res.headers.get('x-filename'));
+    }
+    _click() {
+        if (!this.fss.isFileSaverSupported) {
+            return;
+        }
+        let req = this.http;
+        if (!req) {
+            let params = new _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpParams"]();
+            const query = this.query || {};
+            // tslint:disable-next-line:forin
+            for (const item in query) {
+                params = params.set(item, query[item]);
+            }
+            req = this.httpClient.request(this.method, this.url, {
+                observe: 'response',
+                responseType: 'blob',
+                headers: this.header,
+                params,
+            });
+        }
+        this.setDisabled(true);
+        req.subscribe((res) => {
+            if (res.status !== 200 || res.body.size <= 0) {
+                this.error.emit(res);
+                return;
+            }
+            this.fss.save(res.body, this.getName(res), null, this.fsOptions);
+            this.success.emit(res);
+        }, (err) => this.error.emit(err), () => this.setDisabled(false));
+    }
+    setDisabled(status) {
+        const el = this.el.nativeElement;
+        el.disabled = status;
+        el.classList[status ? 'add' : 'remove'](`filesaver__disabled`);
+    }
+}
+FileSaverDirective.ɵfac = function FileSaverDirective_Factory(t) { return new (t || FileSaverDirective)(_angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdirectiveInject"](_angular_core__WEBPACK_IMPORTED_MODULE_0__["ElementRef"]), _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdirectiveInject"](FileSaverService), _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdirectiveInject"](_angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpClient"])); };
+FileSaverDirective.ɵdir = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineDirective"]({ type: FileSaverDirective, selectors: [["", "fileSaver", ""]], hostBindings: function FileSaverDirective_HostBindings(rf, ctx) { if (rf & 1) {
+        _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵlistener"]("click", function FileSaverDirective_click_HostBindingHandler() { return ctx._click(); });
+    } }, inputs: { method: "method", http: "http", query: "query", header: "header", url: "url", fileName: "fileName", fsOptions: "fsOptions" }, outputs: { success: "success", error: "error" }, exportAs: ["fileSaver"] });
+FileSaverDirective.ctorParameters = () => [
+    { type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["ElementRef"] },
+    { type: FileSaverService },
+    { type: _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpClient"] }
+];
+FileSaverDirective.propDecorators = {
+    method: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"] }],
+    http: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"] }],
+    query: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"] }],
+    header: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"] }],
+    url: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"] }],
+    fileName: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"] }],
+    fsOptions: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"] }],
+    success: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Output"] }],
+    error: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Output"] }],
+    _click: [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["HostListener"], args: ['click',] }]
+};
+/*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](FileSaverDirective, [{
+        type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Directive"],
+        args: [{
+                selector: '[fileSaver]',
+                exportAs: 'fileSaver'
+            }]
+    }], function () { return [{ type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["ElementRef"] }, { type: FileSaverService }, { type: _angular_common_http__WEBPACK_IMPORTED_MODULE_2__["HttpClient"] }]; }, { method: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"]
+        }], success: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Output"]
+        }], error: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Output"]
+        }], _click: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["HostListener"],
+            args: ['click']
+        }], http: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"]
+        }], query: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"]
+        }], header: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"]
+        }], url: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"]
+        }], fileName: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"]
+        }], fsOptions: [{
+            type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["Input"]
+        }] }); })();
+
+class FileSaverModule {
+}
+FileSaverModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineNgModule"]({ type: FileSaverModule });
+FileSaverModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineInjector"]({ factory: function FileSaverModule_Factory(t) { return new (t || FileSaverModule)(); } });
+(function () { (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵsetNgModuleScope"](FileSaverModule, { declarations: [FileSaverDirective], exports: [FileSaverDirective] }); })();
+/*@__PURE__*/ (function () { _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵsetClassMetadata"](FileSaverModule, [{
+        type: _angular_core__WEBPACK_IMPORTED_MODULE_0__["NgModule"],
+        args: [{
+                declarations: [FileSaverDirective],
+                exports: [FileSaverDirective]
+            }]
+    }], null, null); })();
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+
+
+//# sourceMappingURL=ngx-filesaver.js.map
+
+/***/ }),
+
 /***/ "F97/":
 /*!*********************************************************!*\
   !*** ./node_modules/rxjs/_esm2015/internal/util/not.js ***!
@@ -14825,6 +15017,22 @@ function toArray() {
     return Object(_reduce__WEBPACK_IMPORTED_MODULE_0__["reduce"])(toArrayReducer, []);
 }
 //# sourceMappingURL=toArray.js.map
+
+/***/ }),
+
+/***/ "Iab2":
+/*!*******************************************************!*\
+  !*** ./node_modules/file-saver/dist/FileSaver.min.js ***!
+  \*******************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;(function(a,b){if(true)!(__WEBPACK_AMD_DEFINE_ARRAY__ = [], __WEBPACK_AMD_DEFINE_FACTORY__ = (b),
+				__WEBPACK_AMD_DEFINE_RESULT__ = (typeof __WEBPACK_AMD_DEFINE_FACTORY__ === 'function' ?
+				(__WEBPACK_AMD_DEFINE_FACTORY__.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__)) : __WEBPACK_AMD_DEFINE_FACTORY__),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));else {}})(this,function(){"use strict";function b(a,b){return"undefined"==typeof b?b={autoBom:!1}:"object"!=typeof b&&(console.warn("Deprecated: Expected third argument to be a object"),b={autoBom:!b}),b.autoBom&&/^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(a.type)?new Blob(["\uFEFF",a],{type:a.type}):a}function c(a,b,c){var d=new XMLHttpRequest;d.open("GET",a),d.responseType="blob",d.onload=function(){g(d.response,b,c)},d.onerror=function(){console.error("could not download file")},d.send()}function d(a){var b=new XMLHttpRequest;b.open("HEAD",a,!1);try{b.send()}catch(a){}return 200<=b.status&&299>=b.status}function e(a){try{a.dispatchEvent(new MouseEvent("click"))}catch(c){var b=document.createEvent("MouseEvents");b.initMouseEvent("click",!0,!0,window,0,0,0,80,20,!1,!1,!1,!1,0,null),a.dispatchEvent(b)}}var f="object"==typeof window&&window.window===window?window:"object"==typeof self&&self.self===self?self:"object"==typeof global&&global.global===global?global:void 0,a=f.navigator&&/Macintosh/.test(navigator.userAgent)&&/AppleWebKit/.test(navigator.userAgent)&&!/Safari/.test(navigator.userAgent),g=f.saveAs||("object"!=typeof window||window!==f?function(){}:"download"in HTMLAnchorElement.prototype&&!a?function(b,g,h){var i=f.URL||f.webkitURL,j=document.createElement("a");g=g||b.name||"download",j.download=g,j.rel="noopener","string"==typeof b?(j.href=b,j.origin===location.origin?e(j):d(j.href)?c(b,g,h):e(j,j.target="_blank")):(j.href=i.createObjectURL(b),setTimeout(function(){i.revokeObjectURL(j.href)},4E4),setTimeout(function(){e(j)},0))}:"msSaveOrOpenBlob"in navigator?function(f,g,h){if(g=g||f.name||"download","string"!=typeof f)navigator.msSaveOrOpenBlob(b(f,h),g);else if(d(f))c(f,g,h);else{var i=document.createElement("a");i.href=f,i.target="_blank",setTimeout(function(){e(i)})}}:function(b,d,e,g){if(g=g||open("","_blank"),g&&(g.document.title=g.document.body.innerText="downloading..."),"string"==typeof b)return c(b,d,e);var h="application/octet-stream"===b.type,i=/constructor/i.test(f.HTMLElement)||f.safari,j=/CriOS\/[\d]+/.test(navigator.userAgent);if((j||h&&i||a)&&"undefined"!=typeof FileReader){var k=new FileReader;k.onloadend=function(){var a=k.result;a=j?a:a.replace(/^data:[^;]*;/,"data:attachment/file;"),g?g.location.href=a:location=a,g=null},k.readAsDataURL(b)}else{var l=f.URL||f.webkitURL,m=l.createObjectURL(b);g?g.location=m:location.href=m,g=null,setTimeout(function(){l.revokeObjectURL(m)},4E4)}});f.saveAs=g.saveAs=g, true&&(module.exports=g)});
+
+//# sourceMappingURL=FileSaver.min.js.map
 
 /***/ }),
 
