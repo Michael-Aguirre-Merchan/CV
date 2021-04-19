20dedcc8a915f6563ec5c034622d899957a06ea2
Canuto2 <maikol.merchan23@gmail.com>
2021-04-19 12:13:38 -0500

    Chess pieces 2.0

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -19,7 +19,7 @@ export class PlayComponent implements OnInit {
     return Array(n);
   }
 
-  counting (fen:any) {
+  counting(fen: any) {
 
     function countString(str, letter) {
       let count = 0;
@@ -38,23 +38,25 @@ export class PlayComponent implements OnInit {
     var Pnum = 8 - result
     var P = Pnum.toString();
     document.getElementById("P")!.innerHTML = P;
-    let PCount:number = Pnum
+    let PCount: number = Pnum
     while (PCount > 0) {
       var pieceNumber = PCount - 1
       var name = 'WhitePawn' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       PCount -= 1
     }
 
     var result = countString(board, "R");
     var Rnum = 2 - result
     var R = Rnum.toString();
-    document.getElementById("R")!.innerHTML = R;  
-    let RCount:number = Rnum
+    document.getElementById("R")!.innerHTML = R;
+    let RCount: number = Rnum
     while (RCount > 0) {
       var pieceNumber = RCount - 1
       var name = 'WhiteRook' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       RCount -= 1
     }
 
@@ -62,12 +64,12 @@ export class PlayComponent implements OnInit {
     var Nnum = 2 - result
     var N = Nnum.toString();
     document.getElementById("K")!.innerHTML = N;
-    let NCount:number = Nnum
+    let NCount: number = Nnum
     while (NCount > 0) {
       var pieceNumber = NCount - 1
       var name = 'WhiteKnight' + pieceNumber
-      
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       NCount -= 1
     }
 
@@ -75,11 +77,12 @@ export class PlayComponent implements OnInit {
     var Bnum = 2 - result
     var B = Bnum.toString();
     document.getElementById("B")!.innerHTML = B;
-    let BCount:number = Bnum
+    let BCount: number = Bnum
     while (BCount > 0) {
       var pieceNumber = BCount - 1
       var name = 'WhiteBishop' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       BCount -= 1
     }
 
@@ -87,11 +90,12 @@ export class PlayComponent implements OnInit {
     var Qnum = 1 - result
     var Q = Qnum.toString();
     document.getElementById("Q")!.innerHTML = Q;
-    let QCount:number = Qnum
+    let QCount: number = Qnum
     while (QCount > 0) {
       var pieceNumber = QCount - 1
       var name = 'WhiteQueen' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       QCount -= 1
     }
 
@@ -99,11 +103,12 @@ export class PlayComponent implements OnInit {
     var pnum = 8 - result
     var p = pnum.toString();
     document.getElementById("p")!.innerHTML = p;
-    let pCount:number = pnum
+    let pCount: number = pnum
     while (pCount > 0) {
       var pieceNumber = pCount - 1
       var name = 'BlackPawn' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       pCount -= 1
     }
 
@@ -111,11 +116,12 @@ export class PlayComponent implements OnInit {
     var rnum = 2 - result
     var r = rnum.toString();
     document.getElementById("r")!.innerHTML = r;
-    let rCount:number = rnum
+    let rCount: number = rnum
     while (rCount > 0) {
       var pieceNumber = rCount - 1
       var name = 'BlackRook' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       rCount -= 1
     }
 
@@ -123,11 +129,12 @@ export class PlayComponent implements OnInit {
     var nnum = 2 - result
     var n = nnum.toString();
     document.getElementById("k")!.innerHTML = n;
-    let nCount:number = nnum
+    let nCount: number = nnum
     while (nCount > 0) {
       var pieceNumber = nCount - 1
       var name = 'BlackKnight' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       nCount -= 1
     }
 
@@ -135,11 +142,12 @@ export class PlayComponent implements OnInit {
     var bnum = 2 - result
     var b = bnum.toString();
     document.getElementById("b")!.innerHTML = b;
-    let bCount:number = bnum
+    let bCount: number = bnum
     while (bCount > 0) {
       var pieceNumber = bCount - 1
       var name = 'BlackBishop' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       bCount -= 1
     }
 
@@ -147,15 +155,120 @@ export class PlayComponent implements OnInit {
     var qnum = 1 - result
     var q = qnum.toString();
     document.getElementById("q")!.innerHTML = q;
-    let qCount:number = qnum
+    let qCount: number = qnum
     while (qCount > 0) {
       var pieceNumber = qCount - 1
       var name = 'BlackQueen' + pieceNumber
-      document.getElementById(name)!.style.display = "flex"
+      document.getElementById(name)!.classList.remove('no-display');
+      document.getElementById(name)!.classList.add('p-2');
       qCount -= 1
     }
+  }
 
+  deleting() {
+
+    var deletePawns = 7
+    while (deletePawns != -1) {
+      var name = 'WhitePawn' + deletePawns
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deletePawns -= 1
     }
+
+    var deleteRooks = 1
+    while (deleteRooks != -1) {
+      var name = 'WhiteRook' + deleteRooks
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deleteRooks -= 1
+    }
+
+    var deleteKnights = 1
+    while (deleteKnights != -1) {
+      var name = 'WhiteKnight' + deleteKnights
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deleteKnights -= 1
+    }
+
+    var deleteBishops = 1
+    while (deleteBishops != -1) {
+      var name = 'WhiteBishop' + deleteBishops
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deleteBishops -= 1
+    }
+
+    var deleteQueens = 0
+    while (deleteQueens != -1) {
+      var name = 'WhiteQueen' + deleteQueens
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deleteQueens -= 1
+    }
+
+    var deletepawns = 7
+    while (deletepawns != -1) {
+      var name = 'BlackPawn' + deletepawns
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deletepawns -= 1
+    }
+
+    var deleterooks = 1
+    while (deleterooks != -1) {
+      var name = 'BlackRook' + deleterooks
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deleterooks -= 1
+    }
+
+    var deletebishops = 1
+    while (deletebishops != -1) {
+      var name = 'BlackBishop' + deletebishops
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deletebishops -= 1
+    }
+
+    var deleteknights = 1
+    while (deleteknights != -1) {
+      var name = 'BlackKnight' + deleteknights
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deleteknights -= 1
+    }
+
+    var deletequeens = 0
+    while (deletequeens != -1) {
+      var name = 'BlackQueen' + deletequeens
+      if (document.getElementById(name)! != null) {
+        document.getElementById(name)!.classList.remove('p-2');
+        document.getElementById(name)!.classList.add('no-display');
+      }
+      deletequeens -= 1
+    }
+
+
+  }
 
   ngOnInit(): number {
 
@@ -185,11 +298,11 @@ export class PlayComponent implements OnInit {
     var colChess = document.getElementById("col-chess")
 
     setTimeout(() => {
-      
+
       colChess!.style.height = newBoardSize + 30 + 'px'
 
     }, 1);
-    
+
 
     if (screen.width > 767) {
 
@@ -260,12 +373,15 @@ export class PlayComponent implements OnInit {
     document.getElementById("k")!.innerHTML = "0";
     document.getElementById("b")!.innerHTML = "0";
     document.getElementById("q")!.innerHTML = "0";
+    this.fen = this.boardManager.getFEN();
+    this.deleting();
 
   }
 
   undo() {
     this.boardManager.undo();
     this.fen = this.boardManager.getFEN();
+    this.deleting();
     this.counting(this.fen)
   }
 
@@ -276,7 +392,7 @@ export class PlayComponent implements OnInit {
     // console.log(this.fen);
     if (move.x === true) {
 
-      this.counting(this.fen)      
+      this.counting(this.fen)
 
       if (move.color === 'black') {
 
